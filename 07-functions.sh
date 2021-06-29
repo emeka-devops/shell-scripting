72128b07b6c13fec3c0132175d25e40d06c2acb2
emyglobal <emyglobal@users.noreply.github.com>
2021-06-28 22:36:12 -0400

    variable functions

--- 07-functions.sh
+++ 07-functions.sh
@@ -3,13 +3,13 @@
 ##declear a function
 
 SAMPLE() {
-  echo "\e[31mWelcome to red function"
-  echo -e "\e[31mWelcome to red function"
-  echo value of a=$a
+  #echo "\e[31mWelcome to red function"
+  #echo -e "\e[31mWelcome to red function"
+  echo value of a = $a
   b=222
 }
 
 ##access your function
 a=111
 SAMPLE
-value of b=$b
+value of b = $b
